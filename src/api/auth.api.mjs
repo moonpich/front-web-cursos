b7bc740e35b1f77a41f44b1cf23afeba5b64dbc2
Enrique Gonzalez <gonherenrique@gmail.com>
2024-11-29 22:47:44 -0600

    Login logica, lista y tambien register, recovery logica lista

---
+++ src/api/auth.api.mjs
@@ -0,0 +1,72 @@
+const headers = {
+  "Content-Type": "application/json",
+};
+
+export const login = async ({ mail, password }) => {
+  const bodyJson = {
+    email: mail,
+    password: password,
+  };
+
+  const request = await fetch("http://localhost:8080/api/auth/login", {
+    headers: headers,
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(bodyJson),
+  });
+
+  if (!request.ok) {
+    return false;
+  }
+  const responseJson = await request.json();
+  console.log(responseJson);
+
+  return true;
+};
+
+export const register = async ({ name, email, lastname, phone, password }) => {
+  const registerBody = {
+    name: name,
+    lastname: lastname.length === 0 ? "" : lastname,
+    email: email,
+    password: password,
+    phone: phone,
+  };
+
+  const request = await fetch("http://localhost:8080/api/auth/register", {
+    headers: headers,
+    body: JSON.stringify(registerBody),
+    credentials: "include",
+    method: "POST",
+  });
+
+  if (request.status !== 201) {
+    return false;
+  }
+
+  const jsonReponse = await request.json();
+  console.log(jsonReponse);
+  return true;
+};
+
+export const recoveryPasswordByEmail = async ({ email }) => {
+  const objBody = {
+    email: email,
+  };
+  const request = await fetch("/api/auth/recovery", {
+    method: "POST",
+    body: JSON.stringify(objBody),
+    headers: headers,
+    credentials: "include",
+  });
+
+  if (!request.ok) {
+    return false;
+  }
+  const response = await request.json();
+
+  console.log(response);
+
+  return true;
+};
+export default login;
